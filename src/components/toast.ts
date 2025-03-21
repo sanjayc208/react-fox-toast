459d53e39ff81c11bcec49753f0dceb8a76b9855
Robert Brownstein <robert.brownstein@gmail.com>
2025-03-21 19:22:58 -0400

    [#113]: Reduce the use of any types in toast methods

--- src/components/toast.ts
+++ src/components/toast.ts
@@ -7,26 +7,44 @@ import {
   removeAllToast,
   getRemainingTimeForToast,
 } from './toast-store';
+import { ToastProps } from './types';
 
 export const toast = Object.assign(
-  (message: React.ReactNode, options?: Partial<any>) => {
-   return addToast({ ...options, message, type: 'custom' }); // Default to custom toast
+  (
+    message: React.ReactNode,
+    options?: Partial<Omit<ToastProps, 'message'>>
+  ) => {
+    return addToast({ ...options, message, type: 'custom' }); // Default to custom toast
   },
   {
-    success: (message: React.ReactNode, options?: Partial<any>) =>
-      addToast({ ...options, message, type: 'success' }),
-    error: (message: React.ReactNode, options?: Partial<any>) =>
-      addToast({ ...options, message, type: 'error' }),
-    info: (message: React.ReactNode, options?: Partial<any>) =>
-      addToast({ ...options, message, type: 'info' }),
-    envelope: (message: React.ReactNode, options?: Partial<any>) =>
-      addToast({ ...options, message, type: 'envelope' }),
-    drawer: (message: React.ReactNode, options?: Partial<any>) =>
-      addToast({ ...options, message, type: 'drawer' }),
-    warning: (message: React.ReactNode, options?: Partial<any>) =>
-      addToast({ ...options, message, type: 'warning' }),
-    custom: (message: React.ReactNode, options?: Partial<any>) =>
-      addToast({ ...options, message, type: 'custom' }),
+    success: (
+      message: React.ReactNode,
+      options?: Partial<Omit<ToastProps, 'message' | 'type'>>
+    ) => addToast({ ...options, message, type: 'success' }),
+    error: (
+      message: React.ReactNode,
+      options?: Partial<Omit<ToastProps, 'message' | 'type'>>
+    ) => addToast({ ...options, message, type: 'error' }),
+    info: (
+      message: React.ReactNode,
+      options?: Partial<Omit<ToastProps, 'message' | 'type'>>
+    ) => addToast({ ...options, message, type: 'info' }),
+    envelope: (
+      message: React.ReactNode,
+      options?: Partial<Omit<ToastProps, 'message' | 'type'>>
+    ) => addToast({ ...options, message, type: 'envelope' }),
+    drawer: (
+      message: React.ReactNode,
+      options?: Partial<Omit<ToastProps, 'message' | 'type'>>
+    ) => addToast({ ...options, message, type: 'drawer' }),
+    warning: (
+      message: React.ReactNode,
+      options?: Partial<Omit<ToastProps, 'message' | 'type'>>
+    ) => addToast({ ...options, message, type: 'warning' }),
+    custom: (
+      message: React.ReactNode,
+      options?: Partial<Omit<ToastProps, 'message' | 'type'>>
+    ) => addToast({ ...options, message, type: 'custom' }),
     remove: (id: string) => removeToast(id),
     removeAll: () => removeAllToast(),
     update: (id: string, updates: Partial<any>) => updateToast(id, updates),
@@ -34,7 +52,7 @@ export const toast = Object.assign(
     remainingTime: (id: string) => getRemainingTimeForToast(id),
     resume: (id: string) => resumeToastTimer(id),
     promise: (
-      promise: Promise<any>,
+      promise: Promise<void>,
       {
         loading,
         success,
@@ -53,24 +71,24 @@ export const toast = Object.assign(
       // Show the loading toast indefinitely
       const loadingToastId = addToast({
         message: loading,
-        type: "promise",
+        type: 'promise',
         position,
         ...toastOptions,
         duration: Infinity, // Ensure loading toast doesn't disappear
       });
-    
+
       promise
-        .then((res) => {
+        .then(() => {
           updateToast(loadingToastId, {
             message: success,
-            type: "success",
+            type: 'success',
             duration: toastOptions.duration ?? 3000, // Use toastOptions.duration
           });
         })
-        .catch((err) => {
+        .catch(() => {
           updateToast(loadingToastId, {
             message: error,
-            type: "error",
+            type: 'error',
             duration: toastOptions.duration ?? 3000, // Use toastOptions.duration
           });
         });
